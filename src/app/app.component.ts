a6a578d52588a067b8f3b00a0dbe37fac0df6e16
Sivanujan1998 <ssivanujan1998@gmail.com>
2022-09-14 23:49:32 +0530

    Application Developed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from "./app.service";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  inboundClick = true;
   title = 'Demo_Web_App';
+
+  public details: any = [];
+
+  constructor(private service: AppService) {}
+  ngOnInit(): void {
+    
+  }
+
+  FetchUserDetails(): void {
+    this.service.getUsersDetails().subscribe(details => {
+      this.details = details;
+      console.log(details)
+    })
+  }
+
 }
